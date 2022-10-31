ba080c2b632d99d148a756492d2c458f5f8d9e5f
KLIM STOROZHENKO <storozhenko.klim@gmail.com>
2022-10-31 12:08:46 +1000

    Create Scoreboard component

---
+++ src/components/game/scoreboard/Scoreboard.js
@@ -0,0 +1,11 @@
+
+function Scoreboard(props) {
+    return (
+        <div id="scoreboard">
+            <h2 id="current-score">Current score: 0</h2>
+            <h2 id="high-score">High score: 0</h2>
+        </div>
+    );
+}
+
+export default Scoreboard
