528a4b76bfbe9a117d3de82a654f077fdd929337
KLIM STOROZHENKO <storozhenko.klim@gmail.com>
2022-10-31 12:27:41 +1000

    Setup App component layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import Main from "./components/Main";
+import Main from "./components/Main.js";
+import Header from './components/Header.js';
+import Footer from './components/Footer.js';
 
 function App() {
   return (
     <>
+      <Header />
       <Main />
+      <Footer />
     </>
   );
 }
