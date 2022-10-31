a84c9d45555df732a6ef6e5a86019eb67126996e
KLIM STOROZHENKO <storozhenko.klim@gmail.com>
2022-10-31 12:09:19 +1000

    Add props to Scoreboard

--- src/components/game/scoreboard/Scoreboard.js
+++ src/components/game/scoreboard/Scoreboard.js
@@ -2,8 +2,8 @@
 function Scoreboard(props) {
     return (
         <div id="scoreboard">
-            <h2 id="current-score">Current score: 0</h2>
-            <h2 id="high-score">High score: 0</h2>
+            <h2 id="current-score">Current score: {props.current}</h2>
+            <h2 id="high-score">High score: {props.high}</h2>
         </div>
     );
 }
