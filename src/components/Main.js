63fe87d496f6524a9461d66221bdd4a2be2e09b6
KLIM STOROZHENKO <storozhenko.klim@gmail.com>
2022-11-01 10:56:38 +1000

    Create game functionality

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,21 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Cards from '../utils/cards';
 import Scoreboard from "./game/scoreboard/Scoreboard";
 import CardsContainer from './game/cards/CardsContainer';
 
 function Main() {   
-    /**
-     * Generate and array of random cardbacks.
-     * @param {number} amount Amount of cards in array
-     * @returns array of random cardbacks
-     */
-    // const getRandomCards = amount => {
-    //     const copy = Cards.sort(() => Math.random() - 0.5);
-    //     return copy.slice(0, amount);
-    // }
+    const [currentScore, setCurrentScore] = useState(0);
+    const [highScore, setHighScore] = useState(0)
+    const [cards, setCards] = useState(getRandomCards(8));
+    const [rememberedCards, setRememberedCards] = useState([]);
+
+    function getRandomCards(amount) {
+        const copy = Cards.sort(() => Math.random() - 0.5);
+        return copy.slice(0, amount);
+    }
+
+    useEffect(() => {
+        if (currentScore > highScore) {
+            setHighScore(currentScore);
+        }
+    }, [currentScore, highScore])
+
+    const handleCardSelect = e => {
+        const id = e.target.src;
+        if (rememberedCards.indexOf(id) === -1) {
+            setRememberedCards([...rememberedCards, id]);
+            setCurrentScore(currentScore + 1);
+            setCards(getRandomCards(8));
+        } else {
+            setRememberedCards([]);
+            setCurrentScore(0);
+            setCards(getRandomCards(8));
+        }
+    } 
 
     return (<main>
-        <Scoreboard />
+        <Scoreboard current={currentScore} high={highScore} />
+        <CardsContainer cards={cards} onClick={handleCardSelect} />
     </main>);
 }
 
